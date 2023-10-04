37de25168b111f16d79397a6ca6a5c1877e08638
NouraAlotaibi <134725930+NouraAlotaibi@users.noreply.github.com>
2023-10-04 20:33:50 +0300

    Create SensorDataProcessorV2.java

---
+++ SensorDataProcessorV2.java
@@ -0,0 +1,52 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SensorDataProcessor {
+
+    // Sensor data and limits.
+    public double[][][] data;
+    public double[][] limit;
+
+    // Constructor
+    public SensorDataProcessor(double[][][] data, double[][] limit) {
+        this.data = data;
+        this.limit = limit;
+    }
+
+    // Calculates average of sensor data
+    private double average(double[] array) {
+        double val = 0;
+        for (double num : array) {
+            val += num;
+        }
+        return val / array.length;
+    }
+
+    // Calculate data
+    public void calculate(double d) {
+        double[][][] data2 = new double[data.length][data[0].length][data[0][0].length];
+
+        try (BufferedWriter out = new BufferedWriter(new FileWriter("RacingStatsData.txt"))) {
+            for (int i = 0; i < data.length; i++) {
+                for (int j = 0; j < data[0].length; j++) {
+                    for (int k = 0; k < data[0][0].length; k++) {
+                        // Data calculations here
+                        // ...
+                    }
+                }
+            }
+
+            // Properly format and write data2 to the file
+            for (double[][] row : data2) {
+                for (double[] column : row) {
+                    for (double value : column) {
+                        out.write(value + "\t");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
